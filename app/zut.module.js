83623e4b8ee3b74ef54ff1940d2af469096df411
wenkangkang <wenkangkang@istudy.mobi>
2016-11-25 12:13:58 +0800

    progagate

--- app/zut.module.js
+++ app/zut.module.js
@@ -11,32 +11,44 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var router_1 = require('@angular/router');
+var http_1 = require('@angular/http');
 var zut_component_1 = require('./zut.component');
 var guide_component_1 = require('./guide/guide.component');
 var mine_1 = require('./mine/mine'); //我的
 var login_service_1 = require('./mine/login.service');
 var login_1 = require('./mine/login');
+var propagate_list_component_1 = require('./propagate/propagate-list.component');
+var propagate_service_1 = require('./propagate/propagate.service');
+var live_detail_component_1 = require('./propagate/live-detail.component');
 var ZutModule = (function () {
     function ZutModule() {
     }
     ZutModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule,
+            imports: [platform_browser_1.BrowserModule, http_1.HttpModule,
                 router_1.RouterModule.forRoot([
-                    {
-                        path: '',
-                        component: guide_component_1.GuideComponent
-                    },
+                    // {
+                    //     path: '',
+                    //     component: GuideComponent
+                    // },
                     {
                         path: 'login',
                         component: login_1.LoginPage
+                    },
+                    {
+                        path: '',
+                        component: propagate_list_component_1.PropagateListComponent
+                    }, {
+                        path: 'live/:id',
+                        component: live_detail_component_1.LiveDetailComponent
                     }
                 ])
             ],
             providers: [
-                login_service_1.LoginService
+                login_service_1.LoginService,
+                propagate_service_1.PropagateService
             ],
-            declarations: [zut_component_1.ZutComponent, mine_1.Mine, login_1.LoginPage, guide_component_1.GuideComponent],
+            declarations: [zut_component_1.ZutComponent, mine_1.Mine, login_1.LoginPage, guide_component_1.GuideComponent, propagate_list_component_1.PropagateListComponent, live_detail_component_1.LiveDetailComponent],
             bootstrap: [zut_component_1.ZutComponent]
         }), 
         __metadata('design:paramtypes', [])
--- app/zut.module.ts
+++ app/zut.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule }   from '@angular/router';
+import {HttpModule} from '@angular/http';
 
 import { ZutComponent }   from './zut.component';
 import { GuideComponent }   from './guide/guide.component';
@@ -8,23 +9,35 @@ import {Mine} from './mine/mine';//我的
 import { LoginService }      from './mine/login.service';
 import {LoginPage} from './mine/login'
 
+import {PropagateListComponent} from './propagate/propagate-list.component';
+import {PropagateService} from './propagate/propagate.service';
+import {LiveDetailComponent} from './propagate/live-detail.component';
+
 @NgModule({
-  imports:      [ BrowserModule,
+  imports:      [ BrowserModule, HttpModule,
     RouterModule.forRoot([
-        {
-            path: '',
-            component: GuideComponent
-        },
+        // {
+        //     path: '',
+        //     component: GuideComponent
+        // },
         {
           path:'login',
           component:LoginPage
+        },
+        {
+            path:'',
+            component: PropagateListComponent
+        },{
+            path:'live/:id',
+            component:LiveDetailComponent
         }
     ])
   ],
     providers: [
-    LoginService
+    LoginService,
+    PropagateService
   ],
-  declarations: [ ZutComponent,Mine,LoginPage,GuideComponent],
+  declarations: [ ZutComponent,Mine,LoginPage,GuideComponent, PropagateListComponent, LiveDetailComponent],
   bootstrap:[ZutComponent]
 })
 export class ZutModule { }
